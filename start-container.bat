6841ceb19c5e4e101aefe28dd7201dc133bc787b
Jürgen <1manfactory@gmail.com>
2024-09-15 10:40:06 +0200

    readme

--- start-container.bat
+++ start-container.bat
@@ -3,9 +3,11 @@ setlocal
 set "CURRENT_DIR=%cd%"
 docker run -d -p 8080:80 -p 3306:3306 -p 8025:8025 -p 1025:1025 -v "%CURRENT_DIR%/src:/var/www/html" my-fullstack-app
 docker ps
+echo
 echo Container is up and running
 echo phpinfo: http://127.0.0.1:8080/
 echo MailHog: http://127.0.0.1:8025/
-echo "BASH: docker exec -it <container_id> /bin/bash"
+echo 'docker exec -it <container_id> /bin/bash'
+echo
 echo Press any key to exit...
 pause >nul	
